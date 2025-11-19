157c41368cbd04e140aab96e85eb24daa0f2c70d
David Blanco <davidblancopai11.5@gmail.com>
2025-11-19 16:21:20 -0500

    screens

---
+++ app/src/screens/Splash.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+const Splash = ({onFinish}: {onFinish:() => void}) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onFinish();
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, [onFinish]);
+    
+    return(
+        <View style = {styles.container}>
+            <Image 
+            source = {require('../assets/images/arana.png')}
+            style = {styles.logo}
+            resizeMode = 'contain'
+            />
+        </View>
+    );
+
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(240 , 240 ,200 ,1)'
+    },
+    logo: {
+        width: 200,
+        height: 200
+    }
+});
+
+export default Splash;
